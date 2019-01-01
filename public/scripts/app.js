8d64182ea2b2bc0675e540dcb3ddd318cef42bc4
nsharman <nathansharman21@gmail.com>
2018-12-31 18:26:04 -0700

    Arrow functions.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,85 +1,23 @@
 'use strict';
 
-console.log('App.js is running!');
+// function square(x) {
+//     return x * x;
+// }
 
-var app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer.',
-    options: ['One', 'Two']
+// const squareArrow = (x) => {
+//     return x * x;
+// }
+
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(8));
+
+// const getFirstName = (firstName) => {
+//     return firstName.split(' ')[0];
+// }
+
+var getFirstName = function getFirstName(firstName) {
+  return firstName.split(' ')[0];
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item 1'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item 2'
-        )
-    )
-);
-
-var user = {
-    name: 'Nathan',
-    age: 28,
-    location: 'Phoenix'
-};
-
-function getLocation(location) {
-
-    if (location) {
-        return React.createElement(
-            'p',
-            null,
-            'Location: ',
-            location
-        );
-    }
-};
-
-var userName = 'Nathan';
-var userAge = 28;
-var userLocation = 'Phoenix';
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name ? user.name : 'Anonymous'
-    ),
-    user.age && user.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        user.age
-    ),
-    getLocation(user.location)
-);
-
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+console.log(getFirstName('Andrew Mead'));
---
+++ src/playground/es6-arrow-function.js
@@ -0,0 +1,19 @@
+// function square(x) {
+//     return x * x;
+// }
+
+// const squareArrow = (x) => {
+//     return x * x;
+// }
+
+// const squareArrow = (x) => x * x;
+
+// console.log(squareArrow(8));
+
+// const getFirstName = (firstName) => {
+//     return firstName.split(' ')[0];
+// }
+
+const getFirstName = (firstName) => firstName.split(' ')[0];
+
+console.log(getFirstName('Andrew Mead'));
