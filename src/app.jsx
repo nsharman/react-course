e0504dd10f32e8cc03bed4872c1a2b73b2dceca2
nsharman <nathansharman21@gmail.com>
2019-01-27 00:03:09 -0700

    Method binding.

--- src/app.jsx
+++ src/app.jsx
@@ -43,8 +43,14 @@ class Action extends React.Component {
 
 class Options extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.handleRemoveAll = this.handleRemoveAll.bind(this);
+    }
+
     handleRemoveAll() {
-        alert('handleRemoveAll');
+        console.log(this.props.options);
     }
 
     render() {
