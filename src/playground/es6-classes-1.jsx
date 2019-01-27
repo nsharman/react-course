ddb0a0bd37a8a78cd6c3b179568829fe8afe8e9d
nsharman <nathansharman21@gmail.com>
2019-01-26 23:08:14 -0700

    es6 classes part 2.

--- src/playground/es6-classes-1.jsx
+++ src/playground/es6-classes-1.jsx
@@ -15,7 +15,54 @@ class Person {
     }
 }
 
-const me = new Person('Nathan Harman', 28);
+class Student extends Person {
 
-console.log(me.getGreeting());
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+
+        if (this.hasMajor()) {
+            description += `Their major is ${this.major}.`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+
+        if (!!this.homeLocation) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Student('Nathan Harman', 28, 'Computer Science');
 console.log(me.getDescription());
+
+const other = new Student('Nathan Harman', 28);
+console.log(other.getDescription());
+
+const meTraveler = new Traveler('Nathan Harman', 28, 'Phoenix');
+console.log(meTraveler.getGreeting());
+
+const otherTraveler = new Traveler();
+console.log(otherTraveler.getGreeting());
